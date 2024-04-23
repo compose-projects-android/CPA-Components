4b5caf1b6128b50c281fc554a5a2ae7fe02a4c44
Less-parrot <less100100@gmail.com>
2024-04-23 18:30:57 -0500

    v1.0.0

--- cpa_buttons/build.gradle.kts
+++ cpa_buttons/build.gradle.kts
@@ -49,7 +49,7 @@ publishing{
         register<MavenPublication>("release") {
             groupId = "com.github.compose-projects-android"
             artifactId = "CPA-Components"
-            version = "1.0"
+            version = "1.0.0"
             afterEvaluate {
                 from(components["release"])
             }
