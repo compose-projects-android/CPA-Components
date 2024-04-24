36a5d8b373a7099cef9ee48dd3b2af354aef1a18
Less-parrot <less100100@gmail.com>
2024-04-23 21:21:31 -0500

    creación de nuevo módulo texfield

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,12 +51,9 @@ android {
 }
 
 dependencies {
-    //implementation ("com.github.wlprojt:dimage:1.0")
-    //implementation(project(":cpa_textfield"))
-    //implementation("me.zhanghai.compose.preference:library:1.0.0")
-    //implementation("com.github.compose-projects-android:CPA-Components:1.0")
-    implementation(project(":cpa_buttons"))
-    //implementation("com.github.compose-projects-android:CPA-Components:1.0.0")
+
+    implementation("com.github.compose-projects-android:CPA-Components:1.0.1")
+
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.activity.compose)
